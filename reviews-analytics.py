55e3951cbbaeac1c2382cc1b18000458a16471b6
Vicky Yun <marsyunyun1101@gmail.com>
2019-06-06 21:44:00 +0800

    final version

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,13 +1,16 @@
 data = []
 count = 0
+length = []
+line_length = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		length.append(len(line))
+		line_length = line_length + len(line)
 		count += 1
-		if count % 1000 == 0: # 如果看1000求餘數
-			print(len(data))
-print(len(data))
 
-print(data[0])
-print('----------------')
-print(data[1])
+average = line_length / count
+
+print('檔案讀取完了, 總共有',len(data), '筆資料') 
+print('留言平均長度為', average)
