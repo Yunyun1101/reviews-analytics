6cd0979b155ab8ea30f6d21fff7d7a13548ab912
Vicky Yun <marsyunyun1101@gmail.com>
2019-06-06 22:03:39 +0800

    word filter

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -23,3 +23,12 @@ for d in data:
 print('一共有', len(new), '筆長度小於100的資料')
 print(new[0])
 print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
